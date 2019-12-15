b946397e5e7eaf8fcdd0a7035c14c0c68566e0bd
Adam Scarr <adam@vektah.net>
2019-12-15 17:18:47 +1100

    working solver for basic cases

--- src/solver/solver.test.ts
+++ src/solver/solver.test.ts
@@ -1,52 +1,88 @@
-import {
-  clone,
-  flattenInputs,
-  ProductionNode,
-  solve,
-  Template
-} from "./solver";
+import { flattenInputs, Link, ProductionNode, solve, Template } from "./solver";
 import * as util from "util";
+
+const recipes: { [name: string]: Template } = {
+  flour: { inputs: [], outputs: [{ item: "flour", count: 1 }] },
+  advancedFlour: {
+    inputs: [{ item: "water", count: 1 }],
+    outputs: [{ item: "flour", count: 5 }]
+  },
+  water: { inputs: [], outputs: [{ item: "water", count: 1 }] },
+  sugar: { inputs: [], outputs: [{ item: "sugar", count: 1 }] },
+  bread: {
+    inputs: [
+      { item: "water", count: 2 },
+      { item: "flour", count: 3 }
+    ],
+    outputs: [{ item: "bread", count: 1 }]
+  },
+  sweetBread: {
+    inputs: [
+      { item: "water", count: 1 },
+      { item: "flour", count: 1 },
+      { item: "sugar", count: 1 }
+    ],
+    outputs: [{ item: "bread", count: 2 }]
+  }
+};
+
+const allRecipes = Object.values(recipes);
 
 test("solve", () => {
   // add test for unproduceable items
-  const producers: Template[] = [
-    { inputs: [], outputs: [{ item: "flour", count: 1 }], duration: 30 },
-    {
-      inputs: [{ item: "water", count: 1 }],
-      outputs: [{ item: "flour", count: 5 }],
-      duration: 30
-    },
-    { inputs: [], outputs: [{ item: "water", count: 1 }], duration: 30 },
-    { inputs: [], outputs: [{ item: "sugar", count: 1 }], duration: 30 },
-    {
-      inputs: [
-        { item: "water", count: 1 },
-        { item: "flour", count: 1 }
-      ],
-      outputs: [{ item: "bread", count: 1 }],
-      duration: 30
-    },
-    {
-      inputs: [
-        { item: "water", count: 1 },
-        { item: "flour", count: 1 },
-        { item: "sugar", count: 1 }
-      ],
-      outputs: [{ item: "bread", count: 2 }],
-      duration: 30
-    }
-  ];
 
-  const results = solve(producers, { item: "bread", count: 2 });
-  console.log(results);
+  const results = solve(allRecipes, { item: "bread", count: 2 });
+  expect(util.inspect(Array.from(results))).toEqual(`[
+  1 water + 1 flour + 1 sugar -> 2 bread
+    none -> 1 sugar
+    none -> 1 flour
+    none -> 1 water
+  ,
+  1 water + 1 flour + 1 sugar -> 2 bread
+    none -> 1 sugar
+    0.2 water -> 1 flour
+      none -> 0.2 water
+    none -> 1 water
+  ,
+  4 water + 6 flour -> 2 bread
+    none -> 6 flour
+    none -> 4 water
+  ,
+  4 water + 6 flour -> 2 bread
+    1.2 water -> 6 flour
+      none -> 1.2 water
+    none -> 4 water
+  
+]`);
+});
+
+describe("ProductionNode", () => {
+  it("can calculate required input amounts", () => {
+    const node = new ProductionNode(recipes["bread"], [
+      { item: "bread", count: 10 }
+    ]);
+
+    expect(node.requiredInputAmount("flour")).toBe(30);
+    expect(node.requiredInputAmount("water")).toBe(20);
+  });
+
+  it("will request enough materials for all links", () => {
+    const node = new ProductionNode(recipes["bread"], [
+      { item: "bread", count: 10 },
+      { item: "bread", count: 5 }
+    ]);
+
+    expect(node.requiredInputAmount("flour")).toBe(45);
+    expect(node.requiredInputAmount("water")).toBe(30);
+  });
 });
 
 test("flattenInputs", () => {
-  const i1m1: Template = { outputs: [], inputs: [], duration: 101 };
-  const i1m2: Template = { outputs: [], inputs: [], duration: 102 };
-  const i2m1: Template = { outputs: [], inputs: [], duration: 201 };
-  const i2m2: Template = { outputs: [], inputs: [], duration: 202 };
-  const i3m1: Template = { outputs: [], inputs: [], duration: 301 };
+  const i1m1: Template = { outputs: [], inputs: [] };
+  const i1m2: Template = { outputs: [], inputs: [] };
+  const i2m1: Template = { outputs: [], inputs: [] };
+  const i2m2: Template = { outputs: [], inputs: [] };
+  const i3m1: Template = { outputs: [], inputs: [] };
 
   const out = flattenInputs([[i1m1, i1m2], [i2m1, i2m2], [i3m1]]);
   expect(out).toEqual([
@@ -57,46 +93,72 @@ test("flattenInputs", () => {
   ]);
 });
 
-test("clone", () => {
-  const aT: Template = { inputs: [], outputs: [], duration: 1 };
-  const bT: Template = { inputs: [], outputs: [], duration: 2 };
-  const cT: Template = { inputs: [], outputs: [], duration: 3 };
+test("clone incomplete", () => {
+  const bread = new ProductionNode(recipes["bread"], [
+    { item: "bread", count: 2 }
+  ]);
 
-  const a = new ProductionNode(aT);
-  const b = new ProductionNode(bT);
-  const c = new ProductionNode(cT);
-
-  a.destinations.push(b);
-  b.sources.push(a);
-
-  b.destinations.push(c);
-  c.sources.push(b);
-
-  c.destinations.push(a);
-  a.sources.push(c);
-
-  const newGraph = clone(a);
-  expect(newGraph.template.duration).toBe(1);
-  expect(newGraph.destinations[0].template.duration).toBe(2);
-  expect(newGraph.destinations[0].destinations[0].template.duration).toBe(3);
-  expect(newGraph.sources[0].template.duration).toBe(3);
-  expect(newGraph.sources[0].sources[0].template.duration).toBe(2);
-
-  expect(newGraph).not.toBe(a);
+  const [newBread] = ProductionNode.clone([bread]);
+  expect(util.inspect(bread)).toEqual(util.inspect(newBread));
 });
 
-// takes an array in the form
-// using notation i[1]m[2] to denote input 1 for method 2
-// [
-//   [i[1]m[1], i[1]m[2]],
-//   [i[2]m[1], i[2]m[2]],
-//   [i[3]m[1]],
-// ]
-//
-// and turns it into
-// [
-//   [i[1]m[1], i[2]m[1], i[3]m[1]]
-//   [i[1]m[1], i[2]m[2], i[3]m[1]]
-//   [i[1]m[2], i[2]m[1], i[3]m[1]]
-//   [i[1]m[2], i[2]m[2], i[3]m[1]]
-// ]
+test("clone", () => {
+  const bread = new ProductionNode(recipes["bread"], [
+    { item: "bread", count: 2 }
+  ]);
+  const water = new ProductionNode(recipes["water"], [
+    { item: "water", count: 4 }
+  ]);
+  const flour = new ProductionNode(recipes["flour"], [
+    { item: "flour", count: 6 }
+  ]);
+
+  bread.sources[0].destination = water;
+  bread.sources[1].destination = flour;
+
+  flour.destinations[0].destination = bread;
+  water.destinations[0].destination = bread;
+
+  const [newBread, newFlour, newWater] = ProductionNode.clone([
+    bread,
+    flour,
+    water
+  ]);
+
+  expect(newBread.destinations[0].required).toEqual({
+    item: "bread",
+    count: 2
+  });
+
+  expect(newBread.sources[0].required).toEqual({
+    item: "water",
+    count: 4
+  });
+
+  expect(newBread.sources[1].required).toEqual({
+    item: "flour",
+    count: 6
+  });
+
+  expect(newFlour.destinations[0].required).toEqual({
+    item: "flour",
+    count: 6
+  });
+  expect(newWater.destinations[0].required).toEqual({
+    item: "water",
+    count: 4
+  });
+
+  expect(newBread.template).toBe(recipes["bread"]);
+  expect(newBread.sources[0].destination.template).toBe(recipes["water"]);
+
+  expect(newBread.sources[1].destination.template).toBe(recipes["flour"]);
+  expect(
+    newBread.sources[0].destination.destinations[0].destination.template
+  ).toBe(recipes["bread"]);
+  expect(
+    newBread.sources[1].destination.destinations[0].destination.template
+  ).toBe(recipes["bread"]);
+
+  expect(newBread).not.toBe(bread);
+});
--- src/solver/solver.ts
+++ src/solver/solver.ts
@@ -1,10 +1,8 @@
-import { template } from "@babel/core";
 import * as util from "util";
 
 export interface Template {
   inputs: ItemCount[];
   outputs: ItemCount[];
-  duration: number;
 }
 
 export interface ItemCount {
@@ -14,11 +12,18 @@ export interface ItemCount {
 
 export class ProductionNode {
   template: Template;
-  sources?: ProductionNode[] = [];
-  destinations?: ProductionNode[] = [];
+  sources?: Link[];
+  destinations?: Link[];
 
-  constructor(template: Template) {
+  constructor(template: Template, targets: ItemCount[]) {
     this.template = template;
+    this.destinations = targets.map(t => new Link(t));
+    this.sources = this.template.inputs.map(i => {
+      return new Link({
+        item: i.item,
+        count: this.requiredInputAmount(i.item)
+      });
+    });
   }
 
   findUp(name: string): ProductionNode | undefined {
@@ -27,101 +32,223 @@ export class ProductionNode {
     }
 
     for (const d of this.destinations) {
-      const found = d.findUp(name);
+      if (!d.destination) continue;
+      const found = d.destination.findUp(name);
       if (found) {
         return found;
       }
     }
   }
 
+  findConsumed(template: Template): string {
+    for (const consumed of this.template.inputs) {
+      for (const produced of template.inputs) {
+        if (consumed.item == produced.item) {
+          return consumed.item;
+        }
+      }
+    }
+  }
+
+  requiredInputAmount(name: string): number {
+    const total: { [item: string]: number } = {};
+
+    for (const dest of this.destinations) {
+      total[dest.required.item] = total[dest.required.item]
+        ? total[dest.required.item] + dest.required.count
+        : dest.required.count;
+    }
+
+    let maxMultipler = 0;
+    for (const output of this.template.outputs) {
+      const multiplier = total[output.item] / output.count;
+      if (multiplier > maxMultipler) {
+        maxMultipler = multiplier;
+      }
+    }
+
+    const input = this.template.inputs.find(f => f.item == name);
+    if (!input) return 0;
+    return input.count * maxMultipler;
+  }
+
+  static clone(nodes: ProductionNode[]): ProductionNode[] {
+    if (nodes.length == 0) {
+      return [];
+    }
+
+    const seen = new Map<ProductionNode, ProductionNode>();
+    const stack: ProductionNode[] = [nodes[0]];
+
+    const newNode = new ProductionNode(
+      nodes[0].template,
+      nodes[0].destinations.map(d => d.required)
+    );
+    seen.set(nodes[0], newNode);
+
+    while (stack.length) {
+      const v = stack.pop();
+
+      for (let i = 0; i < v.destinations.length; i++) {
+        const old = v.destinations[i];
+        if (!old.destination) continue;
+        let neighbour = seen.get(old.destination);
+        if (!neighbour) {
+          neighbour = new ProductionNode(
+            old.destination.template,
+            old.destination.destinations.map(d => d.required)
+          );
+          stack.push(old.destination);
+          seen.set(old.destination, neighbour);
+        }
+        seen.get(v).destinations[i].destination = neighbour;
+      }
+
+      for (let i = 0; i < v.sources.length; i++) {
+        const old = v.sources[i];
+        if (!old.destination) continue;
+        let neighbour = seen.get(old.destination);
+        if (!neighbour) {
+          neighbour = new ProductionNode(
+            old.destination.template,
+            old.destination.destinations.map(d => d.required)
+          );
+          stack.push(old.destination);
+          seen.set(old.destination, neighbour);
+        }
+        seen.get(v).sources[i].destination = neighbour;
+      }
+    }
+
+    return nodes.map(n => seen.get(n));
+  }
+
+  *walk(): Generator<[ProductionNode, number]> {
+    const seen = new Set<ProductionNode>();
+    const unseen: [ProductionNode, number][] = [[this, 0]];
+    seen.add(this);
+
+    while (unseen.length > 0) {
+      const [node, depth] = unseen.pop();
+      seen.add(node);
+
+      node.sources
+        .filter(s => s.destination && !seen.has(s.destination))
+        .map(s => {
+          unseen.push([s.destination, depth + 1]);
+          seen.add(s.destination);
+        });
+
+      node.destinations
+        .filter(s => s.destination && !seen.has(s.destination))
+        .map(s => {
+          unseen.push([s.destination, depth - 1]);
+          seen.add(s.destination);
+        });
+
+      yield [node, depth];
+    }
+  }
+
   [util.inspect.custom](depth: number, opts: any) {
-    const inputs: string =
-      this.template.inputs.map((i, n) => `${i.count} ${i.item}`).join(" + ") ||
-      "none";
+    let min = 0;
+    for (const [node, depth] of this.walk()) {
+      if (depth < min) {
+        min = depth;
+      }
+    }
 
-    const outputs = this.template.outputs
-      .map(i => `${i.count} ${i.item}`)
-      .join(" + ");
+    let out = "";
+    for (const [node, depth] of this.walk()) {
+      out += "  ".repeat(depth - min);
 
-    var sourceRecipes: string = this.sources
-      .map(s => s[util.inspect.custom](depth + 1, opts))
-      .join("\n" + "  ".repeat(depth));
+      out +=
+        node.sources
+          .map((i, n) => `${i.required.count} ${i.required.item}`)
+          .join(" + ") || "none";
 
-    if (sourceRecipes)
-      sourceRecipes = "\n" + "  ".repeat(depth) + sourceRecipes;
+      out += " -> ";
 
-    return `${inputs} = ${outputs}${sourceRecipes}`;
+      out += node.destinations
+        .map(i => `${i.required.count} ${i.required.item}`)
+        .join(" + ");
+
+      out += "\n";
+    }
+    return out;
   }
 }
 
-export function clone(node: ProductionNode): ProductionNode {
-  const seen = new Map<ProductionNode, ProductionNode>();
-  const stack: ProductionNode[] = [node];
+export class Link {
+  destination?: ProductionNode;
+  required: ItemCount;
 
-  const newNode = new ProductionNode(node.template);
-  seen.set(node, newNode);
-
-  while (stack.length) {
-    const v = stack.pop();
-
-    for (const old of v.destinations) {
-      let neighbour = seen.get(old);
-      if (!neighbour) {
-        neighbour = new ProductionNode(old.template);
-        stack.push(old);
-        seen.set(old, neighbour);
-      }
-
-      seen.get(v).destinations.push(neighbour);
-    }
-
-    for (const old of v.sources) {
-      let neighbour = seen.get(old);
-      if (!neighbour) {
-        neighbour = new ProductionNode(old.template);
-        stack.push(old);
-        seen.set(old, neighbour);
-      }
-
-      seen.get(v).sources.push(neighbour);
-    }
+  constructor(required: ItemCount, destination?: ProductionNode) {
+    this.destination = destination;
+    this.required = required;
   }
-  return newNode;
 }
 
-export function solve(
+export function* solve(
   producers: Template[],
   target: ItemCount
-): ProductionNode[] {
+): Generator<ProductionNode> {
   const producerByInput = createProducerMap(producers);
-  let incomplete = producerByInput[target.item].map(
-    template => new ProductionNode(template)
+  let solutions = producerByInput[target.item].map<ProductionNode[]>(
+    template => {
+      return [new ProductionNode(template, [target])];
+    }
   );
-  let results = new Set<ProductionNode>();
 
-  // let results: ProductionNode[] = [...incomplete];
+  while (solutions.length > 0) {
+    const incomplete = solutions.pop();
 
-  while (incomplete.length > 0) {
-    const node = incomplete.shift();
-    results.delete(node);
-    const inputs = flattenInputs(
-      node.template.inputs.map(i => producerByInput[i.item])
-    );
+    while (incomplete.length > 0) {
+      const peek = incomplete[incomplete.length - 1];
+      const possibleWays = flattenInputs(
+        peek.template.inputs.map(i => producerByInput[i.item])
+      );
 
-    for (let i = 0; i < inputs.length; i++) {
-      const way = inputs[i];
-      let graph = clone(node);
-      results.add(graph.findUp(target.item));
+      if (possibleWays.length == 0) {
+        const consumer = incomplete.pop();
+        if (incomplete.length === 0) {
+          yield consumer.findUp(target.item);
+        }
+        continue;
+      }
 
-      for (const input of way) {
-        const newNode = new ProductionNode(input);
-        graph.sources.push(newNode);
-        newNode.destinations.push(graph);
-        incomplete.push(newNode);
+      const clones = possibleWays.map((_, i) =>
+        i === 0 ? incomplete : ProductionNode.clone(incomplete)
+      );
+
+      solutions.push(...clones.slice(1));
+
+      for (let i = 0; i < possibleWays.length; i++) {
+        const inputs = possibleWays[i];
+        const incomplete = clones[i];
+        const consumer = incomplete.pop();
+
+        for (let j = 0; j < inputs.length; j++) {
+          const inputTemplate = inputs[j];
+          const producer = new ProductionNode(
+            inputTemplate,
+            inputTemplate.outputs.map(t => ({
+              item: t.item,
+              count: consumer.requiredInputAmount(t.item)
+            }))
+          );
+
+          consumer.sources[j].destination = producer;
+          producer.destinations[0].destination = consumer;
+          incomplete.push(producer);
+        }
+
+        if (incomplete.length === 0) {
+          yield consumer.findUp(target.item);
+        }
       }
     }
   }
-  return Array.from(results);
 }
 
 // takes an array in the form
